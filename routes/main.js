7f6eaf4460765a2fe50a592bcaf83644730b8dd8
devrolet <chetdhill@gmail.com>
2020-07-02 09:45:42 -0400

    Templating Engine and Landing Page Config

--- routes/main.js
+++ routes/main.js
@@ -2,10 +2,7 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
 const router = vertex.router();
 
 router.get('/', (req, res) => {
-  const data = {
-    greeting: 'Hello, Welcome to my portfolio site!!',
-    introduction: 'I am a full stack web developer based in Atlanta'
-  }
+  const data = req.context;
   res.render('landing', data);
 });
 
