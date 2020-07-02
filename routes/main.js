35da06ca5c44d7eab04e14c9348bcac5dddff6c6
devrolet <chetdhill@gmail.com>
2020-07-02 14:55:50 -0400

    Set up Project Render from DB

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,21 @@
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
 const router = vertex.router();
 
+const ProjectController = require('../controllers/ProjectController');
+
 router.get('/', (req, res) => {
   const data = req.context;
-  res.render('landing', data);
+
+  const projectCtr = new ProjectController();
+
+  projectCtr.get()
+    .then(projects => {
+      console.log('Projects ' + JSON.stringify(projects));
+      res.render('landing', data);
+    })
+    .catch(err => {
+      res.send('Error: ' + err.message);
+    });
 });
 
 module.exports = router;
