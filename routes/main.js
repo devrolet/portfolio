46abc3f9108063d4950f3c6755c21e2151e86a46
devrolet <chetdhill@gmail.com>
2020-07-02 16:24:26 -0400

    Render Projects, Pull from online DB

--- routes/main.js
+++ routes/main.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
 
   projectCtr.get()
     .then(projects => {
+      data['projects'] = projects;
       console.log('Projects ' + JSON.stringify(projects));
       res.render('landing', data);
     })
