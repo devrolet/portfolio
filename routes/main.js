0ab03f1b01c2ca0c2d77d3b08a1d1f3917059af6
devrolet <chetdhill@gmail.com>
2020-07-06 15:27:30 -0400

    Add Services Dynamically

--- routes/main.js
+++ routes/main.js
@@ -2,21 +2,39 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
 const router = vertex.router();
 
 const ProjectController = require('../controllers/ProjectController');
+const ServiceController = require('../controllers/ServiceController');
 
 router.get('/', (req, res) => {
   const data = req.context;
-
   const projectCtr = new ProjectController();
+  const serviceCtr = new ServiceController();
 
   projectCtr.get()
     .then(projects => {
       data['projects'] = projects;
-      console.log('Projects ' + JSON.stringify(projects));
+      // console.log('Projects ' + JSON.stringify(projects));
       res.render('landing', data);
     })
     .catch(err => {
       res.send('Error: ' + err.message);
     });
+
+    // services controller
+  serviceCtr.get()
+    .then(services => {
+      data.services = services;
+      console.log('Services ' + JSON.stringify(services));
+      // res.render('landing', data);
+    })
+    .catch(err => {
+      res.send('Error: ' + err.message);
+    });
+  // posts controller
+    
+});
+
+router.get('/', (req, res) => {
+  
 });
 
 router.get('/project/:slug', (req, res) => {
@@ -27,7 +45,7 @@ router.get('/project/:slug', (req, res) => {
   projectCtr.get({slug:projectSlug})
     .then(projects => {
       if (projects.length == 0){
-        throw new Error('Project not found')
+        throw new Error('Project not found');
         return
       }
 
@@ -38,6 +56,11 @@ router.get('/project/:slug', (req, res) => {
     .catch(err => {
       res.send('Error -' + err.message);
     });
+});
+
+router.get('/blog', (req, res) => {
+  res.send('<h1>Welcome to the Blog</h1>');
+  // res.render('posts', data);
 });
 
 module.exports = router;
