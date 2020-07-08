244545cee10a6f53ec2a1b2a1fe21651225c0a3c
devrolet <chetdhill@gmail.com>
2020-07-08 09:42:25 -0400

    Fix Landing page connection issues

--- routes/main.js
+++ routes/main.js
@@ -1,48 +1,36 @@
 const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID});
 const router = vertex.router();
 
-const ProjectController = require('../controllers/ProjectController');
-const ServiceController = require('../controllers/ServiceController');
-const PostController = require('../controllers/PostController');
+const controllers = require('../controllers');
+// 
 
 router.get('/', (req, res) => {
   const data = req.context;
-  const projectCtr = new ProjectController();
-  const serviceCtr = new ServiceController();
-  const postCtr = new PostController();
-  // const landingData = res.render('landing', data);
-
-  projectCtr.get()
+  const ctr = new controllers.project();
+  ctr.get()
     .then(projects => {
-      data['projects'] = projects;
-      // console.log('Projects ' + JSON.stringify(projects));
+      data.projects = projects;
+      const servicesCtr = new controllers.service();
+      return servicesCtr.get();
+    })
+    .then(services => {
+      data.services = services;
+      const postsCtr = new controllers.post();
+      return postsCtr.get();
+    })
+    .then(posts => {
+      data.posts = posts;
+      data.post1 = posts[0];
+      data.post2 = posts[1];
+      data.post3 = posts[2];
+
       res.render('landing', data);
     })
     .catch(err => {
-      res.send('Error: ' + err.message);
-    });
-
-  // services controller
-  serviceCtr.get()
-    .then(services => {
-      data.services = services;
-      console.log('Services ' + JSON.stringify(services));
-      // res.render('landing', data);
-    })
-    .catch(err => {
-      res.send('Error: ' + err.message);
-    });
-  // posts controller
-  postCtr.get()
-    .then(posts => {
-      data.posts = posts;
-      data['post1'] = posts[0];
-      data['post2'] = posts[1];
-      data['post3'] = posts[2];
-      console.log('Posts ' + JSON.stringify(posts));
-    })
-    .catch(err => {
-      res.send('Error: ' + err.message);
+      res.json({
+        confirmation: 'fail',
+        message: err.message
+      });
     });
 });
 
@@ -50,7 +38,7 @@ router.get('/project/:slug', (req, res) => {
   const data = req.context;
   const projectSlug = req.params.slug;
 
-  const projectCtr = new ProjectController();
+  const projectCtr = new controllers.project();
   projectCtr.get({slug:projectSlug})
     .then(projects => {
       if (projects.length == 0){
@@ -59,7 +47,7 @@ router.get('/project/:slug', (req, res) => {
       }
 
       const project = projects[0];
-      data['project'] = project;
+      data.project = project;
       res.render('project', data);
     })
     .catch(err => {
@@ -70,7 +58,7 @@ router.get('/project/:slug', (req, res) => {
 router.get('/post/:slug', (req, res) => {
   const data = req.context;
   const postSlug = req.params.slug;
-  const postCtr = new postController();
+  const postCtr = new controllers.post();
 
   postCtr.get({slug:postSlug})
     .then(posts => {
@@ -80,7 +68,7 @@ router.get('/post/:slug', (req, res) => {
       }
 
       const post = posts[0];
-      data['post'] = post;
+      data.post = post;
       res.render('post', data);
     })
     .catch(err => {
@@ -89,8 +77,20 @@ router.get('/post/:slug', (req, res) => {
 });
 
 router.get('/blog', (req, res) => {
-  res.send('<h1>Welcome to the Blog</h1>');
-  // res.render('posts', data);
+  const data = req.context; // {cdn:<STRING>, global:<OBJECT>}
+
+	let ctr = new controllers.post();
+	ctr.get()
+	.then(posts => {
+		data.posts = posts;
+    	res.render('blog', data);
+	})
+	.catch(err => {
+		res.json({
+			confirmation: 'fail',
+			message: err.message
+		});
+	});
 });
 
 module.exports = router;
